fd76b3fc88ba0030c857e49fdf9871d3af7298ca
Amin <amin1382422amin@gmail.com>
2022-12-25 20:30:40 +0330

    fix nameing

---
+++ _1/_1.cs
@@ -0,0 +1,13 @@
+﻿namespace _1
+{
+	internal class _1
+	{
+		static void Main(string[] args)
+		{
+			bool content = true;
+			bool nocontent = false;
+			Console.WriteLine("the test is {0}", content);
+			Console.WriteLine("the test is {0}", nocontent);
+		}
+	}
+}
