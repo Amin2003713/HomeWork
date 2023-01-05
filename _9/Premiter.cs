0f4f274dffc399e1ac509d2121be996216af6e33
Amin <amin1382422amin@gmail.com>
2023-01-05 10:19:53 +0330

    Adding 9th homework

---
+++ _9/Premiter.cs
@@ -0,0 +1,35 @@
+﻿public class Premiter
+{
+	public int Radius { get; set; }
+	public int Rectangel { get; set; }
+	public int Height { get; set; }
+
+	private Premiter() { }
+
+	public Premiter(int radius, int rectangel, int height)
+	{
+		Radius = radius;
+		Rectangel = rectangel;
+		Height = height;
+	}
+
+	public double SquarePremiter()
+	{
+		return Radius * 4;
+	}
+
+	public double Rectangale()
+	{
+		return (Radius + Rectangel) * 2;
+	}
+
+	public double Circle()
+	{
+		return (Radius) * Math.PI;
+	}
+
+	public double Trapezius()
+	{
+		return ((Radius * Rectangel) / 2) * Height;
+	}
+}
---
+++ _9/Program.cs
@@ -0,0 +1,42 @@
+﻿namespace _9
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			Console.WriteLine("inter hight if u have it :");
+			var h = Convert.ToInt32(Console.ReadLine());
+			Console.WriteLine("inter first side of ur shape : ");
+			var rad = Convert.ToInt32(Console.ReadLine());
+			Console.WriteLine("inter seconde side of ur shape : ");
+			var r = Convert.ToInt32(Console.ReadLine());
+			Console.WriteLine("u want the premiter of 1) cirlce 2) rectangle 3) square 4) Trapezius");
+			var choise = Convert.ToInt32(Console.ReadLine());
+
+
+
+
+
+			var primiter = new Premiter(rad, r, h);
+
+			switch (choise)
+			{
+				case 1:
+					Console.WriteLine(primiter.Circle());
+					break;
+				case 2:
+					Console.WriteLine(primiter.Rectangale());
+					break;
+				case 3:
+					Console.WriteLine(primiter.SquarePremiter());
+					break;
+				case 4:
+					Console.WriteLine(primiter.Trapezius());
+					break;
+				default:
+					Console.WriteLine("not Difined");
+					break;
+			}
+		}
+	}
+}
